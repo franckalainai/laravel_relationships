c2659a7af8b9cf0e5e5d7c533d6646add2d21f5a
franckalainai <franckalain.ai@gmail.com>
2020-08-21 16:17:44 +0000

    first commit

---
+++ app/Http/Controllers/CategoryController.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use App\Category;
+
+class CategoryController extends Controller
+{
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
+    public function create()
+    {
+        return view('category.create');
+    }
+
+    public function store(Request $request)
+    {
+        $category = new Category;
+        $category->title = $request->get('title');
+        $category->user()->associate($request->user());
+
+        $category->save();
+
+        return 'Success';
+    }
+}
